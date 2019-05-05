49e20a34c340e868a3b8afa936c762e3745ea10c
namsoo <are34@naver.com>
2019-05-05 15:48:22 +0000

    notice모델 생성

---
+++ app/Model/NOTICE.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notice extends Model
+{
+	protected $table = 'notices';
+	public $timestamps = false;
+	protected $primarhKey = 'id';
+	protected $fillable = ['id', 'time', 'title', 'content']
+}
