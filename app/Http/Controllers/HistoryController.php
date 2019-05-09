f3b6a5c1110d829d8b1dfdce35418ed2bb4229a4
namsoo <are34@naver.com>
2019-05-09 15:38:49 +0000

    history all list

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\HISTORY;
 
 class HistoryController extends Controller
 {
-    //
+	const RESULT_SUCCESS = 777;
+	const RESULT_ERR = 404;
+
+	# all history
+	public function allHistory(){
+		$db = HISTORY::get();
+		if($db->isEmpty()){
+			return array(
+				"result"=>self::RESULT_ERR,
+				"data"=>""
+			);
+		}
+		return array(
+			"result"=>self::RESULT_SUCCESS,
+			"data"=>$db
+		);
+	}
+
+
 }
